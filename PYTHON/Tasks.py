275b402e6c1f99e96f0ab22f5b7956b8bd562bc4
Matin Soleymani <matinsoleymni@gmail.com>
2023-08-11 10:36:53 +0330

    add python

--- PYTHON/Tasks.py
+++ PYTHON/Tasks.py
@@ -1,1 +1,43 @@
-# add be soon 
+import requests 
+
+api = "https://blokchainology.com/api/tasks/"
+# get user data
+data = {
+    "user_id": 7
+}
+result = requests.post(api , json=data)
+print(result.text)
+
+# /* ***************************** */
+
+# add new task 
+data2 = {
+    "user_id": 1,
+    "title" : "Task 2411",
+    "description" : "this is a test description",
+    "completed": "false"
+}
+result = requests.post(api , json=data2)
+print(result.text)
+
+# /* ***************************** */
+
+# Edit new task 
+data3 = {
+    "id" : 60,
+    "user_id": 1,
+    "title" : "Task 2411",
+    "description" : "this is a test description",
+    "completed": "false"
+}
+result = requests.put(api, json=data3)
+print(result.text)
+
+# /* ***************************** */
+
+# delete new task 
+data4 = {
+    "id" : 60
+}
+result = requests.delete( api , json=data4)
+print(result.text)
